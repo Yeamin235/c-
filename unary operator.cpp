d05a70dbac3083452089e5ee75396519ee830023
Yeamin235 <38450577+Yeamin235@users.noreply.github.com>
2018-08-07 01:36:17 +0600

    Add files via upload

---
+++ unary operator.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x=5,y;
+
+    y=x++;
+
+    cout<<y<<"\n"<<x<<endl;
+
+    y=++x;
+
+
+
+        cout<<y<<"\n"<<x<<endl;
+
+
+y=--x;
+
+
+    cout<<y<<"\n"<<x<<endl;
+
+y=x--;
+
+
+    cout<<y<<"\n"<<x<<endl;
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
